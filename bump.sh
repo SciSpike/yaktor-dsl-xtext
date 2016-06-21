fba7ba0be8faca8bb6373098a58a71ad901df56a
JK <jonathan.kamke@gmail.com>
2016-06-21 13:47:18 -0700

    allow setting version

--- bump.sh
+++ bump.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash -xe
 npm install
 cur_version=$(mvn org.apache.maven.plugins:maven-help-plugin:2.1.1:evaluate -Dexpression=project.version -N | egrep -v "(^\[INFO\]|^\[DEBUG\])" | xargs $(npm bin)/semver -i patch)
-next_version=$(echo $cur_version | xargs $(npm bin)/semver -i $1)
+next_version=${VERSION-$(echo $cur_version | xargs $(npm bin)/semver -i $1)}
 echo "$cur_version -> $next_version"
 
 find . -name site.xml | xargs -n 1 sed -i.bak "s/$cur_version/$next_version/g" 
