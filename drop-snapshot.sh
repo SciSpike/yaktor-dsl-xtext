2f2b1c4296ed9bf5eb5f2b5e2f20daa8e5d376ea
Matthew T. Adams <matthew@matthewadams.me>
2016-07-19 15:17:48 -0500

    release testing

--- drop-snapshot.sh
+++ drop-snapshot.sh
@@ -2,13 +2,17 @@
 
 CURRENT=$(mvn help:evaluate -Dexpression=project.version -N | egrep -v '(^\[.*)' | xargs)
 if [[ ! $CURRENT =~ \-SNAPSHOT$ ]]; then
-  echo "WARNING: $CURRENT is NOT a snapshot; borking..." >&2
+  echo "ERROR: $CURRENT is NOT a snapshot; borking..." >&2
   exit 1
 fi
 
 NEXT=$(echo $CURRENT | sed -E 's/\-SNAPSHOT//g')
-
+CURRENT_SED=$(echo -n $NEXT | sed -E 's/\.)/\\./g'")
 set -x
+find . -name site.xml | xargs -n 1 sed -i.drop -E "s/${CURRENT_SED}(\.qualifier)?/$NEXT/g"
+find . -name MANIFEST.MF | xargs -n 1 sed -i.drop -E "s/Bundle-Version: ${CURRENT}\.qualifier/Bundle-Version: ${NEXT}/g"
+find . -name feature.xml | xargs -n 1 sed -i.drop -E "s/version=\"${CURRENT}\.qualifier/version=\"${NEXT}/g"
+find . -name *.drop | xargs rm
 mvn org.codehaus.mojo:versions-maven-plugin:2.1:set \
   -N \
   -DgenerateBackupPoms=false -DnewVersion=${NEXT}
