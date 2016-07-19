fe2d31edf393145a5055d5faaa3612b98af1b751
Matthew T. Adams <matthew@matthewadams.me>
2016-07-19 17:11:25 -0500

    release testing

--- bump.sh
+++ bump.sh
@@ -14,24 +14,21 @@ fi
 NEXT=${VERSION-$(echo $CURRENT | xargs $(npm bin)/semver -i $INCREMENT)}
 CURRENT_SED=$(echo -n $NEXT | sed -E 's/\./\\./g')
 set -x
-for f in $(find . -name site.xml); do
+for f in ./conversation/update.all/site.xml ; do
   cp -f "$f" "$f.bump"
   CONTENT=$(cat "$f")
-  echo "$CONTENT"
-  CONTENT=$(echo -n "$CONTENT" | sed -E "s/_$CURRENT_SED(\\.qualifier){0,1}\.jar/_$NEXT.qualifier.jar/g")
-  echo "$CONTENT"
+  CONTENT=$(echo -n "$CONTENT" | sed -E "s/_$CURRENT_SED(\\.qualifier){0,1}\\.jar/_$NEXT.qualifier.jar/g")
   CONTENT=$(echo -n "$CONTENT" | sed -E "s/version=\"$CURRENT_SED(\\.qualifier){0,1}\"/version=\"$NEXT.qualifier\"/g")
-  echo "$CONTENT"
   CONTENT=$(echo -n "$CONTENT" | sed -E "s/_$CURRENT_SED\"/_$NEXT\"/g")
   echo "$CONTENT"
   echo "$CONTENT" > "$f"
-  rm -f "$f.bump"
 done
-find . -name MANIFEST.MF | xargs -n 1 sed -E -i.bump "s/Bundle\\-Version: ${CURRENT_SED}(\\.qualifier){0,1}/Bundle-Version: ${NEXT}.qualifier/g"
-find . -name feature.xml | xargs -n 1 sed -E -i.bump "s/version=\"${CURRENT_SED}(\\.qualifier){0,1}/version=\"${NEXT}.qualifier/g"
+find conversation/io.yaktor.conversation.update -name site.xml | xargs -n 1 sed -E -i.bump "s/$CURRENT_SED/$NEXT/g"
+find . -name MANIFEST.MF | xargs -n 1 sed -E -i.bump "s/Bundle\\-Version: $CURRENT_SED(\\.qualifier){0,1}/Bundle-Version: $NEXT.qualifier/g"
+find . -name feature.xml | xargs -n 1 sed -E -i.bump "s/version=\"$CURRENT_SED(\\.qualifier){0,1}/version=\"$NEXT.qualifier/g"
 find . -name '*.bump' | xargs rm
 mvn org.codehaus.mojo:versions-maven-plugin:2.1:set \
   -N \
   -DgenerateBackupPoms=false \
-  -DnewVersion=${NEXT}-SNAPSHOT \
-  -DoldVersion=${CURRENT}${SNAPSHOT}
+  -DnewVersion=$NEXT-SNAPSHOT \
+  -DoldVersion=$CURRENT$SNAPSHOT
