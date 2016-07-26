77996fec339e47dd0b577ef8d77d909e54e6f224
Matthew T. Adams <matthew@matthewadams.me>
2016-07-26 01:11:24 -0500

    quote travis enc values

--- cli/travis-before-deploy.sh
+++ cli/travis-before-deploy.sh
@@ -3,5 +3,5 @@ set -e
 
 CLI_DIR=${CLI_DIR:-./cli}
 
-openssl aes-256-cbc -K $encrypted_a314b1bbacc4_key -iv $encrypted_a314b1bbacc4_iv -in "$CLI_DIR/signingkey.asc.enc" -out "$CLI_DIR/signingkey.asc" -d
+openssl aes-256-cbc -K "$encrypted_a314b1bbacc4_key" -iv "$encrypted_a314b1bbacc4_iv" -in "$CLI_DIR/signingkey.asc.enc" -out "$CLI_DIR/signingkey.asc" -d
 gpg --fast-import "$CLI_DIR/signingkey.asc"
