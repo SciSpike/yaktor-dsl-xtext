6bae104b7f6abeabef0f51e5585a35319e1c6e8f
Matthew T. Adams <matthew@matthewadams.me>
2016-07-27 23:47:14 -0500

    automate eclipse update site creation

--- eclipse-update.sh
+++ eclipse-update.sh
@@ -1,27 +1,47 @@
 #!/usr/bin/env bash
+set -ex
 
-if [ -z "$VERSION" ]; then
-  VERSION=$(mvn -f "$(dirname $0)" help:evaluate -Dexpression=project.version | egrep -v '^\[.*' | xargs) # get "downloading" messages out of the way
-  export VERSION=$(mvn -f "$(dirname $0)" help:evaluate -Dexpression=project.version | egrep -v '^\[.*' | xargs) # now egrep will work
-fi
-echo "VERSION=$VERSION"
+# build the update site
+VERSION=$(cd $(dirname $0)/conversation/update.all.tmpl && npm install && node build-update-site.js)
 
-# auth
-set -x
+# config & auth
+AWS_CONFIG_SECTION=${AWS_CONFIG_SECTION:-default}
+AWS_CONFIG_REGION=${AWS_CONFIG_REGION:-us-west-2}
+AWS_CONFIG_OUTPUT=${AWS_CONFIG_OUTPUT:-json}
 AWS_DIR=${AWS_DIR:-$HOME/.aws}
-mkdir -p "$AWS_DIR"
-CREDENTIALS="${CREDENTIALS:-$AWS_DIR/credentials}"
-touch "$CREDENTIALS"
-chmod go-rwx "$CREDENTIALS"
-set +x
-cat <<EOF >> "$CREDENTIALS"
-[scispike]
-aws_access_key_id=$AWS_ACCESS_KEY_ID
-aws_secret_access_key=$AWS_SECRET_ACCESS_KEY
+AWS_CONFIG_FILE="${AWS_CONFIG_FILE:-$AWS_DIR/config}"
+if [ ! -f "$AWS_CONFIG_FILE" ]; then
+  mkdir -p "$AWS_DIR"
+  cat <<EOF > "$AWS_CONFIG_FILE"
+  [$AWS_CONFIG_SECTION]
+  region = $AWS_CONFIG_REGION
+  output = $AWS_CONFIG_OUTPUT
 EOF
+fi
+AWS_CREDENTIALS_FILE="${AWS_CREDENTIALS_FILE:-$AWS_DIR/credentials}"
+if [ ! -f "$AWS_CREDENTIALS_FILE" ]; then
+  mkdir -p "$AWS_DIR"
+  touch "$AWS_CREDENTIALS_FILE"
+  chmod go-rwx "$AWS_CREDENTIALS_FILE"
+  set +x
+  AWS_CREDENTIALS_SECTION=${AWS_CREDENTIALS_SECTION:-scispike}
+  cat <<EOF > "$AWS_CREDENTIALS_FILE"
+  [$AWS_CREDENTIALS_SECTION]
+  aws_access_key_id = $AWS_ACCESS_KEY_ID
+  aws_secret_access_key = $AWS_SECRET_ACCESS_KEY
+EOF
+  set -x
+fi
 
-# go!
-set -x
+# get versions of all previously released update sites
+AWS_S3_PATH=${AWS_S3_PATH:-yaktor.io/eclipse/} # trailing slash is necessary
+EXISTING_VERSIONS=$(aws s3 ls $AWS_S3_PATH | egrep '/\s{0,}$' | awk '{print $2}' | sed 's,/$,,g' | tr '\n' ',' | sed 's/,$//g')
+VERSIONS=$VERSION
+if [ -n "$EXISTING_VERSIONS" ]; then
+  VERSIONS=$EXISTING_VERSIONS,$VERSIONS
+fi
+
+# upload
 npm install
-composite/site.js --sites $VERSION
+composite/site.js --sites $VERSIONS
 composite/upload.sh
