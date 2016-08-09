a36822561094352774b2dd31e5122b8b526e8e00
Matthew T. Adams <matthew@matthewadams.me>
2016-08-09 15:37:41 -0500

    fix cli generate.js

--- cli/src/main/npm/generate.js
+++ cli/src/main/npm/generate.js
@@ -2,7 +2,7 @@
 var args = process.argv.slice(2)
 var path = require('path')
 var cp = require('child_process')
-var artifactId = '${project.artifactId}'
+var artifactId = '${cli.basename}'
 var version = '${project.version}'
 var jar = [ artifactId, version ].join('-') + '.jar'
 cp.spawn('java', [ '-jar', path.resolve(__dirname, jar) ].concat(args), { stdio: 'inherit' })
